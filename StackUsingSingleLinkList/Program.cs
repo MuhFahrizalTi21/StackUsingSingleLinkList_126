842f265071ca8239038ffffdf3937b06f36b3b25
Muhammad  Fahrizal <m.fahrizal.ft21@mail.umy.ac.id>
2022-11-28 16:28:02 +0700

    Add variables and konstuktor

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -18,7 +18,11 @@ namespace StackUsingSingleLinkList
     }
     class Stack
     {
-        
+        node top;
+        public Stack()
+        {
+            top = null;
+        }
     }
     internal class Program
     {
